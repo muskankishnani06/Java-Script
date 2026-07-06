a7ad1016e5949e30795bae29288d0b7a5aefa278
muskankishnani06 <muskankishnani41@gmail.com>
2026-07-06 17:54:58 +0530

    js intro and concept and add js in html

---
+++ 01_add_js_in_HTML/script.js
@@ -0,0 +1,7 @@
+ document.getElementById("heading")
+
+        document.getElementById("btn")
+            .addEventListener("click", function myFunction() {
+
+                document.getElementById("heading").innerHTML = "logged in";
+            });
---
+++ 02_JS_introduction_and_concept/01_var.js
@@ -0,0 +1,16 @@
+var message = "hi there";
+
+function currentActivity() {
+  // var activity = "running"
+  // console.log("activity:- " ,activity)
+
+  if (true) {
+    let activity = "running";
+    console.log("activity:- ", activity);
+  }
+
+    // console.log("activity:- ", activity);
+
+}
+
+currentActivity();
